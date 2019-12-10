d05b6ac3742a358757c285164079f0eeb8b8e3cf
mgjo5899 <mgjo5899@gmail.com>
2019-12-09 23:34:14 -0800

    4.2 complete

--- day4/sol.py
+++ day4/sol.py
@@ -5,20 +5,31 @@ def valid_passwords(lrange, rrange):
         curr_int_str = str(i)
         never_decrease = True
         double = False
+        double_i = -1
 
         for index, d in enumerate(curr_int_str[1:]):
             if int(d) < int(curr_int_str[index]):
                 never_decrease = False
+                break
             elif int(d) == int(curr_int_str[index]):
-                double = True
+                if not double and double_i == -1:
+                    double = True
+                    double_i = index
+                elif double_i == index - 1:
+                    double = False
+                    double_i = index
+            else:
+                double_i = -1
 
         if never_decrease and double:
             rtn_val.append(i)
 
     return rtn_val
             
-lrange, rrange = [int(num) for num in open('input.txt', 'r').readline().strip().split('-')]
+#file_name = 'small_input.txt'
+file_name = 'input.txt'
+lrange, rrange = [int(num) for num in open(file_name, 'r').readline().strip().split('-')]
 pws = valid_passwords(lrange, rrange)
 
-print(pws)
+#print(pws)
 print(len(pws))
